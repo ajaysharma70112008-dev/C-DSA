cc7611f41375030406083fe459db4404a23c6865
ajaysharma70112008-dev <ajaysharma70112008@gmail.com>
2026-08-19 21:12:25 +0530

    Add files via upload

---
+++ array_swap.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    //array creation
+    
+    int size;
+    printf("Enter the size of array:");
+    scanf("%d",&size);
+    int arr[size];
+    for (int i=0;i<size;i++){
+        int p;
+        printf("Enter the value of the element %d: ",i+1);
+        scanf("%d",&p);
+        arr[i]=p;
+    }
+    
+    //array print
+    
+    for (int i=0;i<size;i++)
+        printf("%d",arr[i]);
+    printf("\n");
+    
+    //array swap
+    
+    int temporary;
+    for(int i=0,j=size-1;i<size/2;i++,j--){
+        temporary=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temporary;
+        
+    }
+    
+    //swapped array print
+    
+    for (int i=0;i<size;i++)
+        printf("%d",arr[i]);
+}
