01294fe1dad6f28394aca0cafcbbac68836cb4b4
ajaysharma70112008-dev <ajaysharma70112008@gmail.com>
2026-08-20 20:35:45 +0530

    find the maximum value in an array
    Fix the array input loop to correctly increment index.

--- maximum_element_from_array.c
+++ maximum_element_from_array.c
@@ -8,13 +8,16 @@ int main()
     int arr[a];
     
     //array value putting
+    
     for(int i=0;i<a;i++){
         int ans;
         printf("the value of array element:\n");
         scanf("%d",&ans);
         arr[i]=ans;
     }
+    
     //finding the max value
+    
     int max=arr[0];
     for(int i=0;i<a;i++){
         if (max<arr[i]){
